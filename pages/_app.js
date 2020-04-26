ef8a8921aeec5ac6e2b90c7868c4035e5eb6bc74
bladexr8 <bladexr8@gmail.com>
2020-04-26 13:38:37 +1000

    Server Side Material UI Style Rendering

--- pages/_app.js
+++ pages/_app.js
@@ -13,31 +13,52 @@ A Higher Order Component (HOC) is a function that takes a component
 in this application to wrap pages with a common Header, and pass props
 to pages to control layout or data.
 
+This Component is also wrapped by the Material-UI ThemeProvider to
+support server side style rendering (refer to index.js)
+
 */
 
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/styles';
 import React from 'react';
 import App from 'next/App';
+import { theme } from '../lib/theme';
 import Header from '../components/Header';
 
 class MyApp extends App {
 
   // peform any data loading required by page
   static async getInitialProps({ Component, ctx }) {
-    let pageProps = {};
+    const pageProps = {};
     if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
+      Object.assign(pageProps, await Component.getInitialProps(ctx));
     }
     return { pageProps };
+  }
+
+  componentDidMount() {
+    // Remove the server-side injected styles
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles && jssStyles.parentNode) {
+      jssStyles.parentNode.removeChild(jssStyles);
+    }
   }
 
   // render the Component
   render() {
     const { Component, pageProps } = this.props;
     return (
-      <>
+      <ThemeProvider theme={theme}>
+        {/* ThemeProvider makes the theme available
+            down the React Tree thanks to React
+        Context */}
+        {/* CssBaseline kickstarts an elegant, 
+            consistent, and simple baseline 
+        to build upon */}
+        <CssBaseline />
         <Header {...pageProps} />
         <Component {...pageProps} />
-      </>
+      </ThemeProvider>
     );
   }
 }

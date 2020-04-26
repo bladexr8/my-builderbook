faf00d8a6a2803924d1521ddda5010004902ca0d
bladexr8 <bladexr8@gmail.com>
2020-04-26 10:54:55 +1000

    Improved Header, Shared Component Styles

--- components/Header.js
+++ components/Header.js
@@ -12,11 +12,26 @@
 
 import Link from 'next/Link';
 
+// Material-UI Toolbar - add a toolbar with action items
+import Toolbar from '@material-ui/core/Toolbar';
+
+// Material-UI Grid - create a column grid
+import Grid from '@material-ui/core/Grid';
+
+// Shared Component Styles
+import { styleToolbar } from './SharedStyles';
+
 const Header = () => (
   <div>
-    <Link href="/login">
-      <a style={{ margin: '0px 20px 0px auto'}}>Log In</a>
-    </Link>
+    <Toolbar style={styleToolbar}>
+      <Grid container direction="row" justify="space-around" align="center">
+        <Grid item xs={12} style={{ textAlign: 'right' }}>
+          <Link href="/login">
+            <a style={{ margin: '0px 20px 0px auto' }}>Log In</a>
+          </Link>
+        </Grid>
+      </Grid>
+    </Toolbar>
   </div>
 );
 

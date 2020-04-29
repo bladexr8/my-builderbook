9c6e7cd5b337c229da9a3c33d08067067fb3e0a9
bladexr8 <bladexr8@gmail.com>
2020-04-29 14:41:26 +1000

    Started Book Model

--- server/models/Book.js
+++ server/models/Book.js
@@ -17,11 +17,61 @@ const { Schema } = mongoose;
 
 const mongoSchema = new Schema({
   // parameters
+  name: {
+    type: String,
+    required: true,
+  },
+  slug: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  githubRepo: {
+    type: String,
+    required: true,
+  },
+  githubLastCommitSha: String,
 });
 
 // static methods
 class BookClass {
-  // methods
+  // get a list of books
+  static async list({ offset = 0, limit = 10 }) {
+    const books = await this.find({})
+      .sort({ createdAt: -1 }) // most recent to least recent
+      .skip(offset) // support pagination
+      .limit(limit);
+    return { books };
+  }
+
+  // get a book and it's chapters
+  static async getBySlug({ slug }) {
+    const bookDoc = await this.findOne({ slug });
+    if (!bookDoc) {
+      throw new Error('Book not found');
+    }
+
+    const book = bookDoc.toObject();
+
+    
+    return book;
+  }
+
+  static async add({ name, price, githubRepo }) {
+    // some code
+  }
+
+  static async edit({ id, name, price, githubRepo }) {
+    // some code
+  }
 }
 
 mongoSchema.loadClass(BookClass);
